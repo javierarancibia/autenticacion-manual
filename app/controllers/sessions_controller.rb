c1e3b0942e7d56c0e604396de58479250631e3db
Javier <javier.arancibia.reyes@hotmail.com>
2020-11-09 01:26:42 -0300

    Created sign in and sign up routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,25 +4,25 @@ class SessionsController < ApplicationController
         @user = User.new
     end
 
-    # def create
-    #     @user = User.find_by(
-    #         email: params[:user][:email],
-    #         password_digest: params[:user][:password_digest]
-    #     )
+    def create
+        @user = User.find_by(
+            email: params[:user][:email],
+            password_digest: params[:user][:password_digest]
+        )
 
 
-    #     if @user.present?
-    #         (session[:user_id]) = @user.id
-    #         redirect_to root_path, notice: 'Has iniciado sesion'
-    #     else 
-    #         redirect_to root_path, notice: 'No se pudo iniciar sesion'
-    #     end
-    # end
+        if @user.present?
+             @user.id = (session[:user_id])
+            redirect_to root_path, notice: 'Has iniciado sesion'
+        else 
+            redirect_to root_path, notice: 'No se pudo iniciar sesion'
+        end
+    end
 
 
-    # private
-    # def user_params
-    #     params.require(:user).permit(:name, :email, :password_digest)
-    # end
+    private
+    def user_params
+        params.require(:user).permit(:name, :email, :password_digest)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :stories
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy] 
+  get 'users/sign_up', to: 'users#new', as: 'sign_up'
+  post 'users', to: 'users#create'
+  get 'users/sign_in', to: 'sessions#new', as: 'sign_in'
+
   root 'stories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
